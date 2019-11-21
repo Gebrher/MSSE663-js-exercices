422ad33c3d39719ad73271b1496c7fa04661f175
waregawi1@gmail.com <waregawi1@gmail.com>
2019-11-21 10:36:17 -0600

    Week 3 Assignment completion by Beza A Abi

--- week3/src/app.ts
+++ week3/src/app.ts
@@ -1,1 +1,35 @@
-// Source code here
+export interface Vehicle {
+    maker:string;
+    type:string;
+    year:number;
+    isFord:()=>boolean;
+    getObjects:(vl:Vehicle,v2:Vehicle,v3:Vehicle)=>Vehicle[];
+
+}
+
+export class Car implements Vehicle{
+    model:string;
+    constructor(public maker:string,public type:string,public year:number,model:string){
+        this.maker=maker;
+        this.type=type;
+        this.year=year
+        this.model=model;
+
+    }
+
+    isFord():boolean {
+        return this.maker==="Ford";
+    }
+
+    getObjects(v1:Vehicle,v2:Vehicle,v3:Vehicle):Vehicle[]{
+        return [v1,v2,v3];
+    }
+}
+
+let car1 = new Car('Toyotoa','sedan',2009,'camry');
+let car2 = new Car('Ford','sedan',2002,'taurus');
+let car3 = new Car('Nissan','Suv',2015,'Defender');
+
+console.log('My second car is Ford and it is '+car2.isFord());
+
+console.log('I own the following cars '+ JSON.stringify(car1.getObjects(car1,car2,car3)));
--- week3/src/observable.ts
+++ week3/src/observable.ts
@@ -1,1 +1,10 @@
-// Source code here
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export const myNumbers = [10,20,30,40];
+export const stream1 = of(myNumbers);
+export const stream2 = stream1.pipe(map(val => val ));
+
+stream2.subscribe(val => {
+	console.log(val);
+});
