9571b70cdd510b7ba3b8b5cca5a8206b9a20993f
waregawi1@gmail.com <waregawi1@gmail.com>
2019-11-21 10:36:17 -0600

    Week 3 Assignment completion by Beza A Abi

--- week3/src/observable.spec.ts
+++ week3/src/observable.spec.ts
@@ -1,2 +1,24 @@
+import { stream2 } from './observable';
+
 import { expect } from 'chai';
 import 'mocha';
+
+describe('Testing observable.ts', () => {
+	it('Should put put "10"', () => {
+		stream2.subscribe(val => {
+			expect(val[0]).to.equal(10);
+		});
+    });
+    
+    it('Should put put "20"', () => {
+		stream2.subscribe(val => {
+			expect(val[1]).to.equal(20);
+		});
+    });
+    
+    it('Should put put "30"', () => {
+		stream2.subscribe(val => {
+			expect(val[2]).to.equal(30);
+		});
+	});
+});
