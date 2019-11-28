309ad11b450d983586ba3c4f5d4bb3de02111b3a
waregawi1@gmail.com <waregawi1@gmail.com>
2019-11-28 04:42:36 -0600

    Week 4 assignmetn submition Beza A A

--- week4/src/spread.spec.ts
+++ week4/src/spread.spec.ts
@@ -1,2 +1,42 @@
+import { numbers,refactoredNumbers,refactoredOrginalContenders,originalPersons,
+refactoredOrigianlPersons,refactoredOriginalUpdates2 } from './spread';
 import { expect } from 'chai';
 import 'mocha';
+
+
+describe('Testing spread.ts',()=>{
+    
+    it('numbsers should equal refactoredNumbers',()=>{
+       expect (numbers).to.be.eql(refactoredNumbers);
+
+});
+
+it('Lenght of refactoredOriginalContenders should be 5',()=>{
+    expect (refactoredOrginalContenders).to.be.equal(5);
+
+});
+
+it('orginalPersons should be same as refactored one',()=>{
+    
+   expect (originalPersons).to.be.eql(refactoredOrigianlPersons);
+  
+});
+
+it('myArray should be same as refactored one',()=>{
+     let myArray = [{
+        id: 0,
+        task: 'Do the thing'
+      },
+      {
+        id: 1,
+        task: 'Do the other thing... again'
+      },
+      {
+        id: 2,
+        task: 'Do the last thing'
+      }]
+    expect (myArray).to.be.eql(refactoredOriginalUpdates2);
+   
+ });
+
+});
