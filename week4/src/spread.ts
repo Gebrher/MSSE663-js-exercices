dc3ac691e189cbc41f148da935b9bee1622fdfb7
waregawi1@gmail.com <waregawi1@gmail.com>
2019-11-28 04:42:36 -0600

    Week 4 assignmetn submition Beza A A

--- week4/src/spread.ts
+++ week4/src/spread.ts
@@ -1,19 +1,40 @@
+import { DefaultSerializer } from "v8";
+
 // #1 Array copying
 export const numbers = [1, 2, 3, 4, 5];
 
 export const originalNumbers = numbers.map(number => number);
 console.log(originalNumbers);
 
-// refactor here
+// Start #1 Refactoring
+ 
+export const refactoredNumbers = [...numbers];
+
+console.log(refactoredNumbers);
+
+
+// End #1 Rfactoring 
+
 
 // #2 Combining arrays
 export const winners = ['first'];
 export const runnerUps = ['second', 'third', 'fourth', 'fifth'];
 
-export const originalContenders = runnerUps.unshift(winners[0]);
-console.log(originalContenders);
+//export const originalContenders = runnerUps.unshift(winners[0]);
+//console.log(originalContenders);
 
-// refactor here
+// Start #2 Refactoring
+
+// I have to comment the above two lines otherwise my new refactored method would 
+// run on top of the above line of code change wich brings output of array size 6.
+// Noe, expected array size from the my refactoring shoud be same size out put at the aboave original , which is 5. 
+export const refactoredOrginalContenders = [...runnerUps,...winners].length;
+
+
+console.log(refactoredOrginalContenders);
+
+
+// End #2 Refactoring
 
 // #3 Combining objects
 export const bob = {
@@ -26,7 +47,15 @@ export const kyle = {
 
 export const originalPersons = Object.assign(bob, kyle);
 
-// refactor here
+// Start #3 Refactoring
+console.log(originalPersons);
+
+export const refactoredOrigianlPersons = {...bob,...kyle};
+
+console.log(refactoredOrigianlPersons);
+
+
+// End #3 Refactoring
 
 // #4 Modifying values in arrays of objects
 export const data = [
@@ -49,11 +78,24 @@ export const update = {
   task: 'Do the other thing... again'
 };
 
-export const originalUpdates = data.map(task => {
-  if (task.id === update.id) {
-    return Object.assign(task, update);
-  }
-  return task;
+ export const originalUpdates = data.map(task => {
+   if (task.id === update.id) {
+   return Object.assign(task, update);
+  }   return task;
 });
 
-// refactor here
+// Start #4 Refactoring
+
+// Before refactor output 
+console.log(originalUpdates);
+
+ export const refactoredOriginalUpdates2 = data.map(task => {
+   if (task.id === update.id) {
+   return {...task,...update};
+ }   return task;
+});
+
+console.log(refactoredOriginalUpdates2);
+
+
+// End #4 Refactoring
